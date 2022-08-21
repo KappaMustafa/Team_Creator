6ee73be00944f26eb2b50cd409dd5c0aeb694593
KappaMustafa <steef136@gmail.com>
2022-08-20 21:20:07 -0500

    made prompting work and fixed file structure and fixed class but no tests yet

--- index.js
+++ index.js
@@ -1,44 +1,132 @@
-const inquirer = require('inquirer')
-const jest = require('jest')
-
-class Employee {
-    constructor(name, id, email){
-    this.name = name;
-    this.id = id;
-    this.email= email;
+const inquirer = require('inquirer');
+const fs = require('fs');
+const Manager = require('./lib/Manager');
+const Engineer= require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const builtTeam = []
+const eAnswers = [
+    {
+        type: "input",
+        name: "name",
+        message: "what is the engineer's name?"
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "what is the engineer's id?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "what is the engineer's email?"
+    },
+    {
+        type: "input",
+        name: "github",
+        message: "what is the engineer's github?"
     }
+];
 
-}
-class Manager extends Employee{
-    super()
-    constructor(name, id, email){
-    this.name = name;
-    this.id = id;
-    this.email = email;
+const intAnswers = [
+    {
+        type: "input",
+        name: "name",
+        message: "what is the intern's name?"
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "what is the intern's id?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "what is the intern's email?"
+    },
+    {
+        type: "input",
+        name: "school",
+        message: "what is the intern's school"
     }
-}
+];
 
-class Engineer extends Employee{
-    super()
-    constructor(name, id, email){
-    this.name = name;
-    this.id = id;
-    this.email = email;
+const mAnswers = [
+    {
+        type: "input",
+        name: "name",
+        message: "what is the manager's name?"
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "what is the manager's id?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "what is the manager's email?"
+    },
+    {
+        type: "input",
+        name: "office number",
+        message: "what is the manager's office number?"
     }
+];
+const init = () => { chosenOption() };
+
+// do done func
+
+function engineerFill() {
+    inquirer.prompt(eAnswers)
+    .then(answers => {
+        let newEng = new Engineer(answers.name, answers.id, 
+            answers.email, answers.github)
+            builtTeam.push(newEng)
+            return chosenOption();
+    })
 }
-
-class Intern extends Employee{
-    super()
-    constructor(name, id, email){
-    this.name = name;
-    this.id = id;
-    this.email = email;
-    }
+function internFill() {
+    inquirer.prompt(intAnswers)
+    .then(answers => {
+        let newIntern = new Intern(answers.name, answers.id,
+            answers.email, answers.github)
+            builtTeam.push(newIntern)
+            return chosenOption();
+    })
 }
+function managerFill() {
+    inquirer.prompt(mAnswers)
+    .then(answers => {
+        let newManager = new Manager(answers.name, answers.id, 
+            answers.email, answers.github)
+            builtTeam.push(newManager)
+            return chosenOption();
+    })
+}
+function chosenOption(){
+    inquirer.prompt([{
+        type: 'list', 
+        name: 'option', 
+        message: 'choose something?',
+        choices: [
+            {name: 'engineer', value: 'newEngineer'},
+            {name: 'intern', value: 'newIntern'}, 
+            {name: 'manager', value: 'newManager'},
+            {name: 'done', value: 'done'}
+        ]
+    }])
+    .then( answer => {
+        if (answer.option === 'newEngineer') {
+            engineerFill()
+        } else if (answer.option === 'newIntern') {
+            internFill()
+        } else if (answer.option === 'newManager') {
+            managerFill()
+        }else if(answer.option === 'done'){
+            done()
+        }
+    })
+}
+init();
 
 
-
-const managerOne = new Manager('Jesse', 2222, 'Jesse@idk.com');
-const managerTwo = new Manager('Felix', 3333, 'Felix@RANK1.com');
-const engineerOne = new Engineer('Train', 4444, 'Train@DefoTwisted.com');
-const internOne = new Intern('Gamba', 5555, 'Gamba@Twisted.com');
